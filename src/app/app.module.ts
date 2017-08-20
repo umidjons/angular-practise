04ea1575d980d4a7c65925b2ba669773c616a657
Umid Almatov <almatov.us@gmail.com>
2017-08-20 16:41:50 +0500

    Parent and children communication via a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AstronautComponent } from './astronaut.component';
+import { MissionControlComponent } from './missioncontrol.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AstronautComponent,
+    MissionControlComponent
   ],
   imports: [
     BrowserModule
