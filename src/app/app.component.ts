2842a737285fd27af4694274ac20f0b932408379
Umid Almatov <almatov.us@gmail.com>
2017-08-22 10:20:16 +0500

    Improve with radios to choose a color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  color: string;
+  colors: string[] = ['lightgreen', 'yellow', 'cyan'];
 }
