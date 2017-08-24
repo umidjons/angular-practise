22c81ee9630facc948b2cda57be5b3174f088cef
Umid Almatov <almatov.us@gmail.com>
2017-08-24 18:53:07 +0500

    Reactive Form - nested group + ngOnChanges with setValue()

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { states } from '../data-model';
+import { states, Address, Hero } from '../data-model';
 
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
+export class HeroDetailComponent implements OnInit, OnChanges {
   
   heroForm: FormGroup;
   states = states;
+
+  @Input()
+  hero: Hero;
   
   constructor(private fb: FormBuilder) {
     this.createForm();
@@ -19,15 +22,18 @@ export class HeroDetailComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngOnChanges() {
+    this.heroForm.reset();
+    this.heroForm.setValue({
+      name: this.hero.name,
+      address: this.hero.addresses[0] || new Address()
+    });
+  }
+
   createForm() {
     this.heroForm = this.fb.group({
       name: ['', Validators.required],
-      address: this.fb.group({
-        street: '',
-        city: '',
-        state: '',
-        zip: '',
-      }),
+      address: this.fb.group(new Address()),
       power: '',
       sidekick: ''
     });
